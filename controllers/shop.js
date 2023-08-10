27d1a68fac0573f012cc9cdb96a63cb0e647c1ce
ushair_ansari <ushair.ansari@finclusionsolutions.com>
2023-08-10 21:30:59 +0530

    seq_ep16

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,32 +2,43 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = async (req, res, next) => {
-  const products = await Product.fetchAll();
-  res.render("shop/product-list", {
-    prods: products,
-    pageTitle: "All Products",
-    path: "/products",
-  });
+  try {
+    const products = await Product.findAll();
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.getProduct = async (req, res, next) => {
   const prodId = req.params.productId;
-  const product = await Product.findById(prodId);
-  console.log("🚀 ~ exports.getProduct= ~ product:", product);
-  res.render("shop/product-detail", {
-    product: product,
-    pageTitle: product.title,
-    path: "/products",
-  });
+  try {
+    const product = await Product.findByPk(prodId);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.getIndex = async (req, res, next) => {
-  const products = await Product.fetchAll();
-  res.render("shop/index", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-  });
+  try {
+    const products = await Product.findAll();
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.getCart = (req, res, next) => {
