79967d61dc0a1e93ab49621eeefbb612a680f243
ushair_ansari <ushair.ansari@finclusionsolutions.com>
2023-08-22 00:19:21 +0530

    replace admin CRUD operation with mongoose ODM.

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const mongoConnect = require("./util/database").mongoConnect;
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById("64dfb1af898c367a92f54249");
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  } catch (err) {
-    console.log(err);
-  }
-});
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById("64dfb1af898c367a92f54249");
+//     req.user = new User(user.name, user.email, user.cart, user._id);
+//     next();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  return app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://ushair:Sheeba%40143@cluster0.cnywq1v.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((res) => app.listen(3000));
